85fbffd4a6e34974117aa00925ce8b7ef66fc14c
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-06 11:29:27 +0530

    Create program7.py

---
+++ program7.py
@@ -0,0 +1,13 @@
+n=1
+for i in range(5):
+    for j in range(5):
+        print(n,end=" ")
+        n=n+1
+    print()
+
+#Output:
+#1 2 3 4 5
+#6 7 8 9 10
+#11 12 13 14 15
+#16 17 18 19 20
+#21 22 23 24 25
